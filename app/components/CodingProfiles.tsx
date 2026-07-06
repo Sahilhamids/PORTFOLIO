a2ea5d0b85d1bc8404b9578b82877ecdb475c161
Sahilhamids <ssahil9635@gmail.com>
2026-07-06 12:50:36 +0530

    Restore GitHub calendar color theme

--- app/components/CodingProfiles.tsx
+++ app/components/CodingProfiles.tsx
@@ -293,6 +293,10 @@ export default function CodingProfiles() {
                 <ActivityCalendar 
                   data={ghActivity} 
                   colorScheme="dark"
+                  theme={{
+                    light: ["#ebedf0", "#9be9a8", "#40c463", "#30a14e", "#216e39"],
+                    dark: ["#161b22", "#0e4429", "#006d32", "#26a641", "#39d353"],
+                  }}
                   fontSize={12}
                   blockSize={12}
                   blockMargin={4}
